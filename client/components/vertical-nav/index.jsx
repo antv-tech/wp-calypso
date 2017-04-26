3521e3973572f626fce47b6ba6f205b09ff1d07d
Ben Dwyer <ben@scruffian.com>
2017-04-26 17:05:41 +0100

    Add stylizer

--- client/components/vertical-nav/index.jsx
+++ client/components/vertical-nav/index.jsx
@@ -2,15 +2,24 @@
  * External dependencies
  */
 import React from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+
+/**
+ * Internal dependencies
+ */
+import style from './style';
 
 const VerticalNav = React.createClass( {
 	render() {
+		console.log( style );
+		console.log( style.verticalNav );
 		return (
-			<div className="vertical-nav">
+			<div className={ style.verticalNav }>
+				sdfs
 				{ this.props.children }
 			</div>
 		);
 	}
 } );
 
-export default VerticalNav;
+export default withStyles( style )( VerticalNav );
